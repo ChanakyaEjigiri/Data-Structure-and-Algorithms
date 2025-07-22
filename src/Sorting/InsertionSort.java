7ec0ded9702c300f6868a0d10662c7a765b2915a
chanakya <chanakya@chanakya>
2025-07-22 23:24:30 +0530

    Added Insertion sort

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class InsertionSort {
+	static void insertionSort(int[] arr) {
+		int n=arr.length;
+		for(int i=1;i<n;i++) {
+			int key=arr[i];
+			int j=i-1;
+			while(j>=0 && arr[j]>key) {
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j+1]=key;
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr= {5,4,3,2,1,1,2,3,4,5};
+		insertionSort(arr);
+		for(int num: arr) {
+			System.out.print(num+" ");
+		}
+	}
+
+}
